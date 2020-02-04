4154e7c5dbe647e403fc6d7c79d58723b35fc990
juscas <just.castonguay@gmail.com>
2020-02-04 14:08:33 -0500

    Added snow tracks shaders

---
+++ Ski/Assets/Shaders/DrawWithMouse.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawWithMouse : MonoBehaviour
+{
+    public Camera _camera;
+    public Shader _drawShader;
+
+    private RenderTexture _splatMap;
+    private Material _snowMat, _drawMat;
+    private RaycastHit _hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _drawMat = new Material(_drawShader);
+        _drawMat.SetVector("Color", Color.red);
+
+        _snowMat = GetComponent<MeshRenderer>().material;
+        _splatMap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        _snowMat.SetTexture("_Splat", _splatMap);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out _hit))
+            {
+                _drawMat.SetVector("_Coordinate", new Vector4(_hit.textureCoord.x, _hit.textureCoord.y, 0, 0));
+                RenderTexture temp = RenderTexture.GetTemporary(_splatMap.width, _splatMap.height, 0, RenderTextureFormat.ARGBFloat);
+                Graphics.Blit(_splatMap, temp);
+                Graphics.Blit(temp, _splatMap, _drawMat);
+                RenderTexture.ReleaseTemporary(temp);
+
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 256, 256), _splatMap, ScaleMode.ScaleToFit, false, 1);
+    }
+}
